6650beaa60ea93d48de423c5610ec3e7f309532c
yitongng <yitong.ng.2022@scis.smu.edu.sg>
2024-08-15 00:36:14 +0800

    fixing git problems

---
+++ app2/main.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI, Response, status, HTTPException, Depends
+from fastapi.params import Body
+from typing import List
+import time
+import psycopg2
+from psycopg2.extras import RealDictCursor # to include column names in your SQL query
+from sqlalchemy.orm import Session
+
+from . import models, schemas, utils # imports models.py, schemas.py, utils.py etc...
+from .database import engine, get_db
+from .routers import user, post # imports user.py, post.py from router folder
+
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+# get the routes from post and user in routers folder
+app.include_router(post.router)
+app.include_router(user.router)
+
+# connections to database can fail so use a try statement
+# cursor_factory= RealDictCursor give you the column name
+while True: # keep trying to connect to the database until it passes
+    try:
+        connection = psycopg2.connect(host= 'localhost', database= 'mywebsite', user= 'postgres', 
+                                    password= 'rootuser', cursor_factory= RealDictCursor)
+        # use to execute SQL statements
+        cursor = connection.cursor()
+        print("Database connection was successful ya ya ya")
+        break
+    # store the error variable in error
+    except Exception as error:
+        print("Connecting to database failed")
+        print("Error:", error)
+        # sleep for 2 secs and try again 
+        time.sleep(2) 
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
---
+++ app2/schemas.py
@@ -0,0 +1,44 @@
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+
+# create a schema with pydantic
+# this class extends BaseModel and other classes will extend it
+# this makes the schemas more flexible and reuseable
+# handle user requests
+class PostBase(BaseModel):
+    title: str
+    content: str
+    # set default value to True even if not specified in json input
+    published: bool = True
+
+# handle creation and update of posts as they are fundamentally the same
+# extend PostBase
+class PostCreate(PostBase):
+    # 'pass' makes PostCreate do the same thing as PostBase
+    pass
+
+# Handle sending data to user (response)
+class Post(PostBase):
+    # specify what you want to send back to the user
+    # title, content and published are already inherited from PostBase
+    created_at: datetime
+# by default, pydantic can only read python dictionaries. 
+# This can produce errors if you use it with ORMs like SQLalchemy
+# writing the code below can make it accept non dictionary formats
+    class Config:
+        orm_mode = True
+
+# handle creation of users
+class UserCreate(BaseModel):
+    email: EmailStr # Pydantic type that validates that a string is an email.
+    password: str
+
+# handle response to User based requests
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
+    created_at: datetime
+# By default, pydantic can only read python dictionaries. 
+# This can produce errors if you use it with ORMs like SQLalchemy writing the code below can make it accept non dictionary formats
+    class Config:
+        orm_mode = True
